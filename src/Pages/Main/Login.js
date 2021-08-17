cf60bb620312ea76485975bc035ae364d2383c5e
KANGDONGHWI <rkdehdgnl@naver.com>
2021-08-17 16:13:41 +0900

    refactor: login 컴포넌트 리팩토링

--- src/Pages/Main/Login.js
+++
@@ -1,8 +0,0 @@
-import { userListStorage } from 'store'
-
-export default function Login({ userName, password }) {
-  const user = userListStorage.load().find(
-    user => user.userName === userName && user.password === password
-  );
-  return user;
-}
